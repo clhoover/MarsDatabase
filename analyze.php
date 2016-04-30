8a3c2275412e920043a0ba382797dc7d142fb1c2
Clhoover <clhoover@asu.edu>
2016-04-30 16:03:48 -0700

    analyze + minor tweaks

---
+++ analyze.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once('mysqli_connect.php');
+
+if(isset($_POST['query'])){
+$query = $_POST['query'];
+
+$response = @mysqli_query($dbc, $query);
+
+if($response){
+
+$data = array();
+  while($row = mysqli_fetch_assoc($response))
+  {
+     $data[] = $row;
+  }
+
+$colNames = array_keys(reset($data));
+
+echo "<table>";
+echo"<tr>";
+
+foreach($colNames as $colName)
+{
+	echo"<th>$colName</th>";
+}
+
+echo"</tr>";
+
+foreach($data as $row)
+{
+	echo"<tr>";
+	foreach($colNames as $colName)
+	{
+		echo"<td>".$row[$colName]."</td>";
+	}
+	echo"</tr>";
+}
+
+echo"</table>";
+
+} else {
+
+echo "Couldn\'t issue database query<br />";
+
+echo mysqli_error($dbc);
+
+}
+} else {
+echo "Couldn't process query<br/>";
+}
+
+mysqli_close($dbc);
+
+?>
